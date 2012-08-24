3e6d35b0a7ae385fc667f685a44815fa44cfed6b
Walker Zhao <zhao.c@neusoft.com>
2012-08-24 10:03:32 +0800

    Add 'show time' feature wq Modify main.c . Add 'show time' feature in the main.c. Use time_t and struct tm type.

--- main.c
+++ main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 
 int main()
 {
+    time_t mytime;
+    struct tm *mylocaltime;
+    mytime = time(NULL);
+    mylocaltime = localtime(&mytime);
+    printf("Date:%d-",mylocaltime->tm_year + 1900);
+    printf("%d-",mylocaltime->tm_mon + 1);
+    printf("%d\n",mylocaltime->tm_mday);
+    printf("Time:%d:",mylocaltime->tm_hour);
+    printf("%d:",mylocaltime->tm_min);
+    printf("%d\n",mylocaltime->tm_sec);
     printf("Hello world!\n");
     return 0;
 }
